418cc78e548c688fed34a79fb7e4352c3cc6ca03
Jaylenchan <jaylen.chan@hotmail.com>
2023-02-25 19:46:14 +0800

    feat: text module

--- web/src/main.ts
+++ web/src/main.ts
@@ -7,15 +7,7 @@ import { Picuture } from './editor-plugins'
 const container = new Container()
 container.bind<IEditor>(TYPES.JEditor).to(JEditor).inSingletonScope()
 container.bind<IPlugin>(TYPES.Picture).to(Picuture).inSingletonScope()
-// container.bind<IPlugin>(TYPES.Text).to(Text).inSingletonScope()
-// container.bind<IPlugin>(TYPES.Topic).to(Topic).inSingletonScope()
-// container.bind<IPlugin>(TYPES.Table).to(Table).inSingletonScope()
 
 const editor = container.get<IEditor>(TYPES.JEditor)
 
-editor
-  .usePlugin(container.get(TYPES.Picture))
-  // .usePlugin(container.get('Text'))
-  // .usePlugin(container.get('Topic'))
-  // .usePlugin(container.get('Table'))
-  .run()
+editor.usePlugin(container.get(TYPES.Picture)).run()
