1185dc90bd7905330931f07d3e544ebfb49bb91a
Jaylenchan <jaylen.chan@hotmail.com>
2023-02-26 16:16:09 +0800

    feat: support j|tsx

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,10 +4,12 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import vueJsx from '@vitejs/plugin-vue-jsx'
 
 export default defineConfig({
   plugins: [
     vue(),
+    vueJsx(),
     AutoImport({
       resolvers: [ElementPlusResolver()],
     }),
