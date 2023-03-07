ea2ac1983df67fb40f5a4b44ababe13eae724460
Jaylenchan <jaylen.chan@hotmail.com>
2023-03-08 00:08:40 +0800

    feat: useService impl

---
+++ packages/jeditor-web/src/shared/utils/service.ts
@@ -0,0 +1,23 @@
+import Symbols from 'settings/dependency-type.config'
+
+import { container } from './dependencyInject'
+
+import type BoardService from 'core/boardService'
+import type EditorPluginService from 'core/editorPluginService'
+import type ModelService from 'core/modelService'
+import type PropPanelPluginService from 'core/propPanelPluginService'
+import type PropPanelService from 'core/propPanelService'
+
+export function useService() {
+	return {
+		boardService: container.get<BoardService>(Symbols.BoardService),
+		editorPluginService: container.get<EditorPluginService>(
+			Symbols.EditorPluginService
+		),
+		ModelService: container.get<ModelService>(Symbols.ModelService),
+		propPanelService: container.get<PropPanelService>(Symbols.PropPanelService),
+		propPanelPluginService: container.get<PropPanelPluginService>(
+			Symbols.PropPanelPluginService
+		),
+	}
+}
