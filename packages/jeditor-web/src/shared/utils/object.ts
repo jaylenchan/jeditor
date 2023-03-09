f2bbe0d7cfd865fd732725d32e6407e54238c067
Jaylenchan <jaylen.chan@hotmail.com>
2023-03-09 19:59:48 +0800

    feat: add type UnionToIntersection to make union to intersection

--- packages/jeditor-web/src/shared/utils/object.ts
+++ packages/jeditor-web/src/shared/utils/object.ts
@@ -1,5 +1,21 @@
 import lodash from 'lodash'
 
-export function merge(srcObject: unknown, ...dstObjects: unknown[]) {
-	return lodash.merge(srcObject, ...dstObjects)
+type Spread<T> = T extends Array<infer P>
+	? {
+			[K in keyof P]: P[K]
+	  }
+	: never
+
+type UnionToIntersection<U> = (
+	U extends U ? (x: U) => unknown : never
+) extends (x: infer R) => unknown
+	? R
+	: never
+
+export function merge<T, K extends Array<unknown>>(
+	srcObject: T,
+	...dstObjects: K
+) {
+	return lodash.merge(srcObject, ...dstObjects) as T &
+		UnionToIntersection<Spread<K>>
 }
